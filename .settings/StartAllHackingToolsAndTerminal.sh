f91d978024088c0daaa9f7309bf83c1ae3fc779b
Zubi <103808711+ZubiHunter@users.noreply.github.com>
2024-12-28 00:13:19 +0500

    Update StartAllHackingToolsAndTerminal.sh

--- .settings/StartAllHackingToolsAndTerminal.sh
+++ .settings/StartAllHackingToolsAndTerminal.sh
@@ -4,12 +4,12 @@ cd /data/data/com.termux/files/usr/etc/
 rm -rf zshrc
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
 cd Tool
 cp zshrc /data/data/com.termux/files/usr/etc/
 cd
 cd
-echo "cd && cd AllHackingTools && python2 MainMenu.py" > sTa4t.txt
+echo "cd && cd AllHuntingTools && python2 MainMenu.py" > sTa4t.txt
 cat "sTa4t.txt" >> /data/data/com.termux/files/usr/etc/zshrc
 rm -rf sTa4t.txt
 cd
