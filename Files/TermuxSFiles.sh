81aa55fb1d552dba08e739c22ae117476402768a
Zubi <103808711+ZubiHunter@users.noreply.github.com>
2024-12-28 00:41:08 +0500

    Update TermuxSFiles.sh

--- Files/TermuxSFiles.sh
+++ Files/TermuxSFiles.sh
@@ -2,21 +2,21 @@ apt install unstable-repo
 apt install telegram-cli
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/adi1090x/termux-style
 cd termux-style
 ./install
 clear
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/laraib07/TermuxBackupTools
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/mishakorzik/qiq
 cd qiq
 bash install.sh
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
