0dfffefcf884512a676f820443e8f3449a33d1ea
Zubi <103808711+ZubiHunter@users.noreply.github.com>
2024-12-27 23:10:14 +0500

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin
-#Copyright 2021 AllHackingTools
-#Written by : Misha Korzhik
+#Copyright 2021 AllHuntingTools
+#Written by : Zubi Hunter
 #Github     : http://github.com/mishakorzik
 
 echo '
@@ -11,8 +11,8 @@ echo '
  |  ██╔══██║██╔══██║██║░░██╗██╔═██╗░██║██║╚████║██║░░╚██╗╚════╝░░░██║░░░██║░░██║██║░░██║██║░░░░░
  |  ██║░░██║██║░░██║╚█████╔╝██║░╚██╗██║██║░╚███║╚██████╔╝░░░░░░░░░██║░░░╚█████╔╝╚█████╔╝███████╗
  |  ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░░░░░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝ v2.8.1
-[3] Title    : AllHackingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
+[3] Title    : AllHuntingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
 [3] Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
-[2] Coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
+[2] Coded By : Zubi Hunter   -  All materials are provided for educational purposes only! 
 [4]——————-——-–—————-—-–—————-————-—–————-———-—–—————————-——–—————–-——[715]
 '|lolcat -p 1.6 -a -d 2 -s 20.0
