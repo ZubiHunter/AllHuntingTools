2359e568bf27ba93b4d0ade82ac0914644051113
Zubi <103808711+ZubiHunter@users.noreply.github.com>
2024-12-28 00:40:11 +0500

    Update SpamFiles.sh

--- Files/SpamFiles.sh
+++ Files/SpamFiles.sh
@@ -3,23 +3,23 @@ apt install paramiko
 apt install pysnmp
 apt install pycrypto
 cd
-cd AllHackingTools
+cd AllHuntingTools
 pip install -r requirements.txt
 pip install requests colorama
 git clone https://github.com/3UMOBKA/SMS-Bomber-300-Free
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/MaksPV/AresBomb
 cd 
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/HACK3RY2J/Anon-SMS.git
 cd Anon-SMS
 cd
-cd AllHackingTools 
+cd AllHuntingTools 
 cd Files
 cd 
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/FSystem88/spymer
 pkg install dos2unix
 pip install requests colorama proxyscrape
@@ -29,9 +29,9 @@ chmod -R 777 ~/spymer
 chmod 777 /bin/spymer
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/TheSpeedX/TBomb.git
 cd TBomb
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
