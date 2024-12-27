1f9eb230f48a818aa6141d7f74e503c62c08080e
Zubi <103808711+ZubiHunter@users.noreply.github.com>
2024-12-28 00:38:59 +0500

    Update SQLinjectionFiles.sh

--- Files/SQLinjectionFiles.sh
+++ Files/SQLinjectionFiles.sh
@@ -2,23 +2,23 @@ apt-get install git
 apt-get install python
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/UndeadSec/Debinject.git
 cd Debinject
 cd
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/Terror696/Vitus2.0
 cd Vitus2.0
 apt-get install python 2.x
 apt-get install dpkg
 apt-get install dpkg-deb
-cd AllHackingTools 
+cd AllHuntingTools 
 cd Files
 cd 
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/mishakorzik/Infect
 cd Infect
 bash install.sh
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
