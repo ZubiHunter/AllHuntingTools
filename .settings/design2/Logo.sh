3c55ce3c099010d089b9186487f99f4c95f0b2a5
Zubi <103808711+ZubiHunter@users.noreply.github.com>
2024-12-28 00:08:04 +0500

    Update Logo.sh

--- .settings/design2/Logo.sh
+++ .settings/design2/Logo.sh
@@ -7,8 +7,8 @@ echo '
  | ██║##██║██║##██║╚█████╔╝██║#╚██╗██║██║#╚███║╚██████╔╝##########██║###╚█████╔╝╚█████╔╝███████╗
  | ╚═╝##╚═╝╚═╝##╚═╝#╚════╝#╚═╝#╚═╝╚═╝╚═╝##╚══╝#╚═════╝############╚═╝####╚════╝##╚════╝#╚══════╝ v2.8
 {h}
-{e}Title    : AllHackingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
-{l}Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
-{l}coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
+{e}Title    : AllHuntingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
+{l}Github   : https://github.com/ZubiHunter  -  nsi⁣ble responsible for any consequences of reading it. 
+{l}coded By : Zubi Hunter          -  All materials are provided for educational purposes only! 
 {o}——————-——-–—————-—-–—————-————-—–————-———-—–—————————-——–—————–-——{+}
 '|lolcat -p 2.6 -a -d 2 -s 20.0
