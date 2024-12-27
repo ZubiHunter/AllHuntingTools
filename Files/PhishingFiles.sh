082fa307b994c97367cb974aa4f73494480fb917
Zubi <103808711+ZubiHunter@users.noreply.github.com>
2024-12-28 00:38:00 +0500

    Update PhishingFiles.sh

--- Files/PhishingFiles.sh
+++ Files/PhishingFiles.sh
@@ -5,51 +5,51 @@ pip install pyngrok
 pip install pgrep
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/htr-tech/zphisher.git
 cd
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/AbirHasan2005/ShellPhish
 cd ShellPhish 
 chmod +x * 
 cd 
-cd AllHackingTools 
+cd AllHuntingTools 
 git clone https://github.com/hangetzzu/saycheese
 cd saycheese
 cd
 cd
-cd AllHackingTools 
+cd AllHuntingTools 
 git clone https://github.com/iinc0gnit0/BlackPhish
 cd BlackPhish
 ./install.sh
 cd 
 cd 
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/mishakorzik/Mask-Phish.Termux
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/thewhiteh4t/seeker.git
 cd seeker
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/DeepSociety/AIOPhish
 cd AIOPhish
 bash install-termux.sh
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/Viralmaniar/I-See-You.git
 cd I-See-You
 chmod u+x ISeeYou.sh
 cd
 cd 
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/An0nUD4Y/blackeye
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
 pip3 install pyphisher
 
 
