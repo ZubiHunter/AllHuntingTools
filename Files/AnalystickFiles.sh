5ab87f65fde72e475128df4aebf67d758321bc01
Zubi <103808711+ZubiHunter@users.noreply.github.com>
2024-12-28 00:29:29 +0500

    Update AnalystickFiles.sh

--- Files/AnalystickFiles.sh
+++ Files/AnalystickFiles.sh
@@ -1,17 +1,17 @@
 cd
 cd 
-cd AllHackingTools
+cd AllHuntingTools
 pip2 install termcolor
 git clone https://github.com/floriankunushevci/rang3r
 cd
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/TechnicalMujeeb/TM-scanner.git 
 cd TM-scanner 
 chmod +x * sh install.sh
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
 git clone https://github.com/Gameye98/AstraNmap
 cd
 cd
-cd AllHackingTools
+cd AllHuntingTools
